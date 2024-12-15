40c08f32882b98cedbc3e24a87877d0d6f355ffa
JolyRed <jolywolf.22341@mail.ru>
2024-12-15 03:05:14 +0100

    формы для отправки сообщений

--- UrbanDjango/UrbanDjango/urls.py
+++ UrbanDjango/UrbanDjango/urls.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from task4 import views
+from task5 import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.main_page, name='main_page'),
-    path('shop/', views.shop_page, name='shop_page'),
-    path('cart/', views.cart_page, name='cart_page'),
+    path('', views.sing_up_by_html, name='sign_up_by_html'),
+    path('django_sign_up/', views.sigh_up_by_django, name='sign_up_by_django')
 ]
---
+++ UrbanDjango/task5/views.py
@@ -0,0 +1,52 @@
+from django.shortcuts import render
+from .forms import UserRegister
+from django.http import HttpResponse
+
+# Create your views here.
+def sigh_up_by_django(request):
+    users = ['login1', 'user1', 'admin']
+    info = {'form': UserRegister()}
+
+    if request.method == "POST":
+        form = UserRegister(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            repeat_password = form.cleaned_data['repeat_password']
+            age = form.cleaned_data['age']
+
+            if password == repeat_password and age >= 18 and username not in users:
+                return HttpResponse(f'Приветствуем, {username}!')
+            else:
+                if password != repeat_password:
+                    info['error'] = 'Пароли не совпадают'
+                elif age < 18:
+                    info['error'] = 'Вам должно быть больше 18 лет'
+                elif username in users:
+                    info['error'] = 'Такой пользователь уже существует'
+        else:
+            info['form'] = form 
+
+    return render(request, 'fifth_task/registration_page.html', info) 
+
+
+def sing_up_by_html(request):
+    users = ['login1', 'user1', 'admin']
+    info = {}
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        repeat_password = request.POST.get('repeat_password')
+        age = request.POST.get('age')
+
+        if password != repeat_password:
+            info['error'] = 'Пароли не совпадают'
+        elif int(age) < 18:
+            info['error'] = 'Вы должны быть старше 18 лет'
+        elif username in users:
+            info['error'] = 'Такой пользователь уже существует'
+        else:
+            return HttpResponse(f'Приветствуем, {username}!')
+
+    return render(request, 'fifth_task/registration_page.html', {'info': info})
