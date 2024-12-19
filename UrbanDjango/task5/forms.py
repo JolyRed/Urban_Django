0836343b56a3583ee21f59067675e16e3531762f
JolyRed <jolywolf.22341@mail.ru>
2024-12-19 16:51:25 +0100

    доработка задания с формами для отправки данных

--- UrbanDjango/task5/forms.py
+++ UrbanDjango/task5/forms.py
@@ -5,4 +5,4 @@ class UserRegister(forms.Form):
     username = forms.CharField(max_length=30, label='Введите логин:')
     password = forms.CharField(widget=forms.PasswordInput, min_length=8, label='Введите пароль:')
     repeat_password = forms.CharField(widget=forms.PasswordInput, min_length=8, label='Повторите пароль:')
-    age = forms.CharField(max_length=3, label='Введите возраст:')
+    age = forms.IntegerField(max_value=120, min_value=0, label='Введите возраст:')
